959f8d8498d5ce57d05aeb3e1dc966276fd126f1
EzraJ <ezraprogramming@gmail.com>
2022-03-26 16:33:30 -0800

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "ping-pong"
+	configurations {"Release", "Debug"}
+
+project "ping-pong"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	includedirs {"include/", "src/"}
+	files {"src/main.cpp", "src/**.cpp", "src/**.c", "include/**.h", "include/**.hpp", "src/**.h", "src/**.hpp"}
+
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		optimize "On"
